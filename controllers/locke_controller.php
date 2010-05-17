d5e720f5cfcba48edfa91f36220def8895a52e01
Scott Mattocks <scott@crisscott.com>
2010-05-17 16:37:59 +0000

    Calculate stats correctly.

--- controllers/locke_controller.php
+++ controllers/locke_controller.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * This file is part of WaterSpout.
- * 
+ *
  * WaterSpout is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * WaterSpout is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with WaterSpout.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -374,10 +374,11 @@ class Locke_Controller extends Controller
 	 */
 	private function _log_response(HTTPResponse $response)
 	{
+		$response->set_default_headers();
 		// Tally up the total bytes sent.
 		if ($this->request instanceof wsrequest)
 		{
-			self::$_ws_bytes_sent+= strlen($response);
+			self::$_ws_bytes_sent+= strlen($response->get_body());
 		}
 		else
 		{
